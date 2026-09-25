f4118ada22c582e38195737cccef3753c77a167e
Vaishali <2004sharmaanshika@gmail.com>
2026-09-25 13:36:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function daysOfAYear (year){
+	if(year % 400 == 0)
+		return 365;
+	else if(year % 4 == 0 && year % 100 != 0)
+		return 365;
+	else{
+		count<<366;
+	}
+}
+const no_of_days = daysOfAYear(year);
+console.log(no_of_days);
+
